ec8de47197e0d3bbc5226b7001ddb1380ea8e66b
IgorYudintsev <kakvospitat@gmail.com>
2024-04-20 12:20:34 +0300

    20.04.2024-20.04.2024-12:11-Sasha+Igor

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Igor} from "./Igor";
 
 function App() {
-  return (
-    <div className="App">
-     <div>20.04.2024-11:22-IGOR</div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>20.04.2024-11:22-MAIN</div>
+            <div>20.04.2024-12:03-MAIN</div>
+            <div>20.04.2024-12:04-MAIN</div>
+            <Igor/>
+        </div>
+    );
 }
 
 export default App;
--- src/Sasha.tsx
+++ src/Sasha.tsx
@@ -6,6 +6,7 @@ export const Sasha = (props: Props) => {
     return (
         <div>
             <div>20.04.2024-12:00-Sasha</div>
+            <div>20.04.2024-12:19-Sasha</div>
         </div>
     );
 };
