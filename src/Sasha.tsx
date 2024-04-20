680b98681fd274b853213c83d5ddb3b268be2c89
IgorYudintsev <kakvospitat@gmail.com>
2024-04-20 12:00:31 +0300

    20.04.2024-11:58-Create Sasha

---
+++ src/Sasha.tsx
@@ -0,0 +1,11 @@
+// @flow
+import * as React from 'react';
+
+type Props = {};
+export const Sasha = (props: Props) => {
+    return (
+        <div>
+            <div>20.04.2024-12:00-Sasha</div>
+        </div>
+    );
+};
