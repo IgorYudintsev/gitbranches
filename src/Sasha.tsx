e433b6abf582ac76f7e810a505cdf9dff802cf8f
IgorYudintsev <kakvospitat@gmail.com>
2024-04-20 12:22:16 +0300

    20.04.2024-20.04.2024-12:22-Sasha

--- src/Sasha.tsx
+++ src/Sasha.tsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 type Props = {};
 export const Sasha = (props: Props) => {
     return (
-        <div>
+        <div style={{backgroundColor:'red'}}>
             <div>20.04.2024-12:00-Sasha</div>
             <div>20.04.2024-12:19-Sasha</div>
             <div>20.04.2024-12:21-Sasha</div>
