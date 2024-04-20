5109399d26b0e0829db8ea646099f60fb5a689b7
IgorYudintsev <kakvospitat@gmail.com>
2024-04-20 11:56:22 +0300

    20.04.2024-11:56-IGOR

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Igor} from "./Igor";
 
 function App() {
   return (
     <div className="App">
      <div>20.04.2024-11:22-IGOR</div>
+        <Igor/>
     </div>
   );
 }
--- src/Igor.tsx
+++ src/Igor.tsx
@@ -1,10 +1,9 @@
-// @flow
 
 import React from "react";
 
 export const Igor = () => {
     return (
-        <div>
+        <div style={{backgroundColor:'green'}}>
             <div>20.04.2024-11:46-IGOR</div>
             <div>20.04.2024-11:53-IGOR</div>
             </div>
