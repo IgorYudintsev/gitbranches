4d75102b1cbdff09c7c69b64fc57f8369ceac3e0
DilanBars2017 <developeryudintsev@gmail.com>
2024-04-21 13:51:37 +0300

    21.04.2024-13:50-Sasha

---
+++ src/NewSasha.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+type Props = {};
+export const NewSasha = (props: Props) => {
+    return (
+        <div style={{backgroundColor:'yellow'}}>
+            <div>21.04.2024-13:50-Sasha</div>
+        </div>
+    );
+};
