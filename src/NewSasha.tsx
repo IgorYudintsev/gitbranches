2a12c6119106a2c4a93dd1c607d32eda34da6618
DilanBars2017 <developeryudintsev@gmail.com>
2024-04-21 14:05:31 +0300

    21.04.2024-14:04-new Sasha 2

--- src/NewSasha.tsx
+++ src/NewSasha.tsx
@@ -4,7 +4,10 @@ type Props = {};
 export const NewSasha = (props: Props) => {
     return (
         <div style={{backgroundColor:'yellow'}}>
-        <div>only one commit</div>
+            <div>21.04.2024-13:50-Sasha</div>
+            <div>21.04.2024-13:51-Sasha</div>
+            <div>21.04.2024-13:59-Sasha</div>
+            <div>21.04.2024-14:04-Sasha</div>
         </div>
     );
 };
